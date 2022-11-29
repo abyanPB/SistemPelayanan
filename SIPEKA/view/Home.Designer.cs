fdc228bd0c235aadc969bf07d59c0b6fe8e09a48
Abyan_PB <abyanpb@gmail.com>
2022-11-29 11:16:42 +0700

    push

--- SIPEKA/view/Home.Designer.cs
+++ SIPEKA/view/Home.Designer.cs
@@ -28,54 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.logout_btn = new System.Windows.Forms.Button();
+            this.aplikasi_btn = new System.Windows.Forms.Button();
+            this.lainnya_btn = new System.Windows.Forms.Button();
+            this.master_data_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nama_lbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlContent1 = new System.Windows.Forms.Panel();
+            this.poli_btn = new System.Windows.Forms.Button();
+            this.dokter_btn = new System.Windows.Forms.Button();
+            this.obat_btn = new System.Windows.Forms.Button();
+            this.pengguna_btn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlControl = new System.Windows.Forms.Panel();
             this.minimize_btn = new System.Windows.Forms.Button();
             this.close_btn = new System.Windows.Forms.Button();
-            this.panel2.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.pnlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlContent1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.pnlTop.SuspendLayout();
+            this.pnlControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.Color.Green;
+            this.pnlLeft.Controls.Add(this.logout_btn);
+            this.pnlLeft.Controls.Add(this.aplikasi_btn);
+            this.pnlLeft.Controls.Add(this.lainnya_btn);
+            this.pnlLeft.Controls.Add(this.master_data_btn);
+            this.pnlLeft.Controls.Add(this.label2);
+            this.pnlLeft.Controls.Add(this.nama_lbl);
+            this.pnlLeft.Controls.Add(this.pictureBox1);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(322, 1080);
+            this.pnlLeft.TabIndex = 1;
+            // 
+            // logout_btn
+            // 
+            this.logout_btn.BackColor = System.Drawing.Color.Transparent;
+            this.logout_btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.logout_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logout_btn.Image = ((System.Drawing.Image)(resources.GetObject("logout_btn.Image")));
+            this.logout_btn.Location = new System.Drawing.Point(0, 1030);
+            this.logout_btn.Name = "logout_btn";
+            this.logout_btn.Size = new System.Drawing.Size(322, 50);
+            this.logout_btn.TabIndex = 7;
+            this.logout_btn.Text = "Log Out";
+            this.logout_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.logout_btn.UseVisualStyleBackColor = false;
+            // 
+            // aplikasi_btn
+            // 
+            this.aplikasi_btn.BackColor = System.Drawing.Color.Transparent;
+            this.aplikasi_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.aplikasi_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aplikasi_btn.Image = ((System.Drawing.Image)(resources.GetObject("aplikasi_btn.Image")));
+            this.aplikasi_btn.Location = new System.Drawing.Point(0, 520);
+            this.aplikasi_btn.Name = "aplikasi_btn";
+            this.aplikasi_btn.Size = new System.Drawing.Size(322, 61);
+            this.aplikasi_btn.TabIndex = 6;
+            this.aplikasi_btn.Text = "Tentang Aplikasi";
+            this.aplikasi_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.aplikasi_btn.UseVisualStyleBackColor = false;
+            // 
+            // lainnya_btn
+            // 
+            this.lainnya_btn.BackColor = System.Drawing.Color.Transparent;
+            this.lainnya_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lainnya_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lainnya_btn.Image = ((System.Drawing.Image)(resources.GetObject("lainnya_btn.Image")));
+            this.lainnya_btn.Location = new System.Drawing.Point(0, 414);
+            this.lainnya_btn.Name = "lainnya_btn";
+            this.lainnya_btn.Size = new System.Drawing.Size(322, 61);
+            this.lainnya_btn.TabIndex = 5;
+            this.lainnya_btn.Text = "Lainnya";
+            this.lainnya_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.lainnya_btn.UseVisualStyleBackColor = false;
+            // 
+            // master_data_btn
+            // 
+            this.master_data_btn.BackColor = System.Drawing.Color.Transparent;
+            this.master_data_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.master_data_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.master_data_btn.Image = ((System.Drawing.Image)(resources.GetObject("master_data_btn.Image")));
+            this.master_data_btn.Location = new System.Drawing.Point(0, 310);
+            this.master_data_btn.Name = "master_data_btn";
+            this.master_data_btn.Size = new System.Drawing.Size(322, 61);
+            this.master_data_btn.TabIndex = 4;
+            this.master_data_btn.Text = "MASTER DATA";
+            this.master_data_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.master_data_btn.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MAIN MENU    ";
+            // 
+            // nama_lbl
+            // 
+            this.nama_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nama_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nama_lbl.Location = new System.Drawing.Point(12, 163);
+            this.nama_lbl.Name = "nama_lbl";
+            this.nama_lbl.Size = new System.Drawing.Size(297, 51);
+            this.nama_lbl.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(109, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlContent1
+            // 
+            this.pnlContent1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlContent1.Controls.Add(this.poli_btn);
+            this.pnlContent1.Controls.Add(this.dokter_btn);
+            this.pnlContent1.Controls.Add(this.obat_btn);
+            this.pnlContent1.Controls.Add(this.pengguna_btn);
+            this.pnlContent1.Controls.Add(this.panel1);
+            this.pnlContent1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent1.Location = new System.Drawing.Point(322, 0);
+            this.pnlContent1.Name = "pnlContent1";
+            this.pnlContent1.Size = new System.Drawing.Size(1598, 1080);
+            this.pnlContent1.TabIndex = 3;
+            // 
+            // poli_btn
+            // 
+            this.poli_btn.AutoSize = true;
+            this.poli_btn.Image = ((System.Drawing.Image)(resources.GetObject("poli_btn.Image")));
+            this.poli_btn.Location = new System.Drawing.Point(404, 710);
+            this.poli_btn.Name = "poli_btn";
+            this.poli_btn.Size = new System.Drawing.Size(210, 230);
+            this.poli_btn.TabIndex = 4;
+            this.poli_btn.Text = "POLI";
+            this.poli_btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.poli_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.poli_btn.UseVisualStyleBackColor = true;
+            // 
+            // dokter_btn
+            // 
+            this.dokter_btn.Image = ((System.Drawing.Image)(resources.GetObject("dokter_btn.Image")));
+            this.dokter_btn.Location = new System.Drawing.Point(1050, 335);
+            this.dokter_btn.Name = "dokter_btn";
+            this.dokter_btn.Size = new System.Drawing.Size(210, 230);
+            this.dokter_btn.TabIndex = 3;
+            this.dokter_btn.Text = "DOKTER";
+            this.dokter_btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.dokter_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.dokter_btn.UseVisualStyleBackColor = true;
+            this.dokter_btn.Click += new System.EventHandler(this.dokter_btn_Click);
+            // 
+            // obat_btn
+            // 
+            this.obat_btn.AutoSize = true;
+            this.obat_btn.Image = ((System.Drawing.Image)(resources.GetObject("obat_btn.Image")));
+            this.obat_btn.Location = new System.Drawing.Point(1050, 710);
+            this.obat_btn.Name = "obat_btn";
+            this.obat_btn.Size = new System.Drawing.Size(210, 230);
+            this.obat_btn.TabIndex = 2;
+            this.obat_btn.Text = "OBAT";
+            this.obat_btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.obat_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.obat_btn.UseVisualStyleBackColor = true;
+            // 
+            // pengguna_btn
+            // 
+            this.pengguna_btn.AutoSize = true;
+            this.pengguna_btn.Image = ((System.Drawing.Image)(resources.GetObject("pengguna_btn.Image")));
+            this.pengguna_btn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.pengguna_btn.Location = new System.Drawing.Point(404, 335);
+            this.pengguna_btn.Name = "pengguna_btn";
+            this.pengguna_btn.Size = new System.Drawing.Size(210, 230);
+            this.pengguna_btn.TabIndex = 1;
+            this.pengguna_btn.Text = "PENGGUNA";
+            this.pengguna_btn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.pengguna_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.pengguna_btn.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Indigo;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.BackColor = System.Drawing.Color.GreenYellow;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(162, 500);
-            this.panel1.TabIndex = 1;
+            this.panel1.Size = new System.Drawing.Size(1598, 196);
+            this.panel1.TabIndex = 0;
             // 
-            // panel3
+            // label3
             // 
-            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(162, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(788, 500);
-            this.panel3.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(445, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(753, 38);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "SISTEM INFORMASI PELAYANAN  KESEHATAN";
             // 
-            // panel2
+            // pnlTop
             // 
-            this.panel2.BackColor = System.Drawing.Color.Silver;
-            this.panel2.Controls.Add(this.panel4);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(162, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(788, 41);
-            this.panel2.TabIndex = 4;
+            this.pnlTop.BackColor = System.Drawing.Color.Silver;
+            this.pnlTop.Controls.Add(this.pnlControl);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(322, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(1598, 41);
+            this.pnlTop.TabIndex = 4;
             // 
-            // panel4
+            // pnlControl
             // 
-            this.panel4.BackColor = System.Drawing.Color.Black;
-            this.panel4.Controls.Add(this.minimize_btn);
-            this.panel4.Controls.Add(this.close_btn);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel4.Location = new System.Drawing.Point(702, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(86, 41);
-            this.panel4.TabIndex = 0;
+            this.pnlControl.BackColor = System.Drawing.Color.Black;
+            this.pnlControl.Controls.Add(this.minimize_btn);
+            this.pnlControl.Controls.Add(this.close_btn);
+            this.pnlControl.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlControl.Location = new System.Drawing.Point(1512, 0);
+            this.pnlControl.Name = "pnlControl";
+            this.pnlControl.Size = new System.Drawing.Size(86, 41);
+            this.pnlControl.TabIndex = 0;
             // 
             // minimize_btn
             // 
@@ -88,6 +276,7 @@
             this.minimize_btn.TabIndex = 2;
             this.minimize_btn.Text = "-";
             this.minimize_btn.UseVisualStyleBackColor = false;
+            this.minimize_btn.Click += new System.EventHandler(this.minimize_btn_Click);
             // 
             // close_btn
             // 
@@ -100,34 +289,55 @@
             this.close_btn.TabIndex = 0;
             this.close_btn.Text = "X";
             this.close_btn.UseVisualStyleBackColor = false;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.ClientSize = new System.Drawing.Size(950, 500);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.ControlBox = false;
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlTop);
+            this.Controls.Add(this.pnlContent1);
+            this.Controls.Add(this.pnlLeft);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.panel2.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
+            this.pnlLeft.ResumeLayout(false);
+            this.pnlLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlContent1.ResumeLayout(false);
+            this.pnlContent1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.Panel pnlContent1;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Panel pnlControl;
         private System.Windows.Forms.Button minimize_btn;
         private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nama_lbl;
+        private System.Windows.Forms.Button aplikasi_btn;
+        private System.Windows.Forms.Button lainnya_btn;
+        private System.Windows.Forms.Button master_data_btn;
+        private System.Windows.Forms.Button logout_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button poli_btn;
+        private System.Windows.Forms.Button dokter_btn;
+        private System.Windows.Forms.Button obat_btn;
+        private System.Windows.Forms.Button pengguna_btn;
     }
 }
